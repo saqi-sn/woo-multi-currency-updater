e4e4bbce48845f0d418163ef3440132976f53154
Ali Sahafi <sa18sahafi@gmail.com>
2025-12-23 02:32:26 +0330

    Correct versions

--- multi-currency-updater-for-woo.php
+++ multi-currency-updater-for-woo.php
@@ -11,7 +11,7 @@
  * Requires at least: 5.8
  * Requires PHP: 7.4
  * WC requires at least: 5.0
- * WC tested up to: 8.0
+ * WC tested up to: 10.3
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
